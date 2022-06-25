114bdea9e09b3b48e096767a59fa2d0137a92fdb
Oleksiy Slavych <alexseeko15@gmail.com>
2022-06-25 12:39:51 +0300

    some changes

--- labs/src/test/java/lab16/TrelloCRUDTest.java
+++ labs/src/test/java/lab16/TrelloCRUDTest.java
@@ -10,62 +10,30 @@ import java.util.UUID;
 import static io.restassured.RestAssured.given;
 
 public class TrelloCRUDTest {
-    private final String API_KEY = "classified";
-    private final String API_TOKEN = "classified";
-    private final String ORGANIZATION_ID = "e90b0487f91148400a38394ee2efb92f";
-    private String boardName;
-    private String boardID;
-
-    @BeforeTest
-    void init() {
-        boardName = UUID.randomUUID().toString();
-    }
-
-    // Create board
-    @Test(priority = 1)
-    void createTest() {
-        Response response = given()
-                .when()
+    private static final String CREATE_BOARD_URL = "https://api.trello.com/1/boards/?name=%s&key=%s&token=%s";
+    private static final String GET_BOARD_URL = "https://api.trello.com/1/boards/%s?key=%s&token=%s";
+    private static final String BOARD_NAME = "username-co";
+    private static final String KEY = "e90b0487f91148400a38394ee2efb92f";
+    private static final String TOKEN = "052c806882b4afd61bb6b2458d18a18d64eababf51c3e6c9a10d6caee1342b0f";
+    private String boardId;
+    @Test
+    public void createBoard() {
+        Response response = given().when()
                 .contentType("application/json")
-                .body(String.format("{\"idOrganization\":\"%s\"}", ORGANIZATION_ID))
-                .post(String.format("https://api.trello.com/1/boards/?name=%s&key=%s&token=%s", boardName, API_KEY, API_TOKEN));
-
-        response
-                .then()
+                .log().all()
+                .post(String.format(CREATE_BOARD_URL, BOARD_NAME, KEY, TOKEN));
+        response.then()
                 .assertThat()
                 .statusCode(200);
-
-        boardID = response.getBody().jsonPath().get("id");
-    }
-    // Update board
-    @Test(priority = 3)
-    void updateBoard() {
-        if (boardID == null) {
-            Assert.fail("Board id is null, make sure you ran first test");
-        }
-
-        given()
-                .when()
-                .queryParam("name", "Changed Board Name")
-                .pathParam("id", boardID)
-                .put(String.format("https://api.trello.com/1/boards/{id}?key=%s&token=%s", API_KEY, API_TOKEN))
-                .then()
-                .assertThat()
-                .statusCode(200);
-
+        boardId = response.getBody().jsonPath().get("id");
     }
 
-    // Delete board
-    @Test(priority = 4)
-    void deleteBoard() {
-        if (boardID == null) {
-            Assert.fail("Board id is null, make sure you ran first test");
-        }
-
-        given()
-                .when()
-                .pathParam("id", boardID)
-                .delete(String.format("https://api.trello.com/1/boards/{id}?key=%s&token=%s", API_KEY, API_TOKEN))
+    @Test(dependsOnMethods = "createBoard")
+    public void getBoard() {
+        given().when()
+                .contentType("application/json")
+                .log().all()
+                .get(String.format(GET_BOARD_URL, boardId, KEY, TOKEN))
                 .then()
                 .assertThat()
                 .statusCode(200);
